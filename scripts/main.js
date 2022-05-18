83077a80e18b0d33ded7eee90827ecf29ba23371
Ankit Rajput <ankitrajput1902@gmail.com>
2022-05-18 17:16:07 +0530

    Added new contact me

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
+function setHeights() {
+  var heights = [];
+  $(".carousel-item").each(function () {
+    heights.push($(this).height());
+  });
+
+  $(".carousel-item").height(Math.max(...heights));
+}
+
+setHeights();
+
 // Card Scroll
 
 $(function () {
